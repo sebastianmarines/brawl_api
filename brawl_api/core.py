ad34e6c2c2394c4d9ec406941c07e0e958c11f91
Sebastian Marines <sebastian0marines@gmail.com>
2021-09-17 17:52:16 -0500

    Add docstrings to methods

--- brawl_api/core.py
+++ brawl_api/core.py
@@ -7,6 +7,8 @@ from brawl_api.models import Battlelog, Player, Club, MemberList
 
 
 class Client:
+    """Client class
+    """
     session: Session
 
     def __init__(self, token: str) -> None:
@@ -20,22 +22,54 @@ class Client:
             return session.get(API.base + route, headers=self.headers)
 
     def get_player(self, player_tag) -> Player:
+        """Get player
+
+        Args:
+            player_tag (str): Brawl stars player tag
+
+        Returns:
+            Player: A Player object
+        """
         player_tag = format_tag(player_tag)
         json_resp = self._request(f"{API.players}/{player_tag}").json()
         return Player.parse_obj(json_resp)
 
     def get_player_battlelog(self, player_tag) -> Battlelog:
+        """Get player's battlelog
+
+        Args:
+            player_tag (str): Brawl stars player tag
+
+        Returns:
+            Battlelog: A Battlelog object
+        """
         player_tag = format_tag(player_tag)
         json_resp = self._request(
             f"{API.players}/{player_tag}/battlelog").json()
         return Battlelog.parse_obj(json_resp["items"])
 
     def get_club(self, club_tag) -> Club:
+        """Get club information
+
+        Args:
+            club_tag (str): Brawl Stars club tag
+
+        Returns:
+            Club: A Club object
+        """
         club_tag = format_tag(club_tag)
         json_resp = self._request(f"{API.club}/{club_tag}").json()
         return Club.parse_obj(json_resp)
 
     def get_club_members(self, club_tag) -> MemberList:
+        """Get club members
+
+        Args:
+            club_tag (str): Brawl Stars club tag
+
+        Returns:
+            MemberList: A MemberList object
+        """
         club_tag = format_tag(club_tag)
         json_resp = self._request(f"{API.club}/{club_tag}/members").json()
         return MemberList.parse_obj(json_resp["items"])
